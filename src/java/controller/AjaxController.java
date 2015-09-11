b33458b6b0c876bea94e0f6601d8b26d04bf1df7
blaze <blaze@gleywson-note>
2015-09-10 23:03:37 -0300

    Finalmente o recurso de trabalhar com tabelas editáveis

---
+++ src/java/controller/AjaxController.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author blaze
+ */
+@ManagedBean
+@RequestScoped
+public class AjaxController {
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+    
+}
