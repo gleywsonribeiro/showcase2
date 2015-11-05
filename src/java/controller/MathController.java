cbc2c5c7cf1b4eccc6beebeb935ef0b9b81c9811
Gleywson <Gleywson@10.79.103.87>
2015-11-05 14:50:42 -0300

    Inserção de fatorial complexo

---
+++ src/java/controller/MathController.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import modelo.Fatorial;
+import modelo.enumeradores.TipoFat;
+
+/**
+ *
+ * @author Gleywson
+ */
+@ManagedBean
+@RequestScoped
+public class MathController {
+    private Fatorial fatorial;
+    private final Map tiposFat;
+    
+
+    public MathController() {
+        this.fatorial = new Fatorial();
+        tiposFat = new HashMap();
+        tiposFat.put("Iterativo", TipoFat.ITERATIVO);
+        tiposFat.put("Recursivo", TipoFat.RECURSIVO);
+    }
+
+    public Map getTiposFat() {
+        return tiposFat;
+    }
+    
+    
+
+    public Fatorial getFatorial() {
+        return fatorial;
+    }
+
+    public void setFatorial(Fatorial fatorial) {
+        this.fatorial = fatorial;
+    }
+    
+    
+    
+}
