c45c6000b69e545b6bab58bfd1ab3f6e3640c90f
gleyw <gleyw@192.168.1.5>
2016-06-29 22:09:30 -0300

    ajustes

--- src/java/beans/Terminal.java
+++ src/java/beans/Terminal.java
@@ -5,7 +5,9 @@
  */
 package beans;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
 
@@ -20,15 +22,20 @@ public class Terminal {
     public String comando(String command, String[] params) {
         switch (command) {
             case "greet":
-                if(params.length > 0)
-                    return "Tudo bem " + params[0] +"?";
-                else
-                    return "Fala teu nome aí rapá! Depois do comando greet!";
+                if (params.length > 0) {
+                    if (params[0].equals("jaynni")) {
+                        return "Oi " + params[0] + ", amiga!";
+                    } else {
+                        return "Tudo bem " + params[0] + "?";
+                    }
+                } else {
+                    return "Digite seu nome depois do comando greet!";
+                }
             case "date":
                 return new Date().toString();
             default:
                 return command + " ->Hum! Ainda não entendo isso!";
         }
     }
-    
+
 }
